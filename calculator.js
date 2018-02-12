97b90d0da5a3f18fb4b75d82c887cf6e80616d5b
FTeddy <ainsophoure@gmail.com>
2018-02-12 11:14:53 +0700

    calculator v.01

--- calculator.js
+++ calculator.js
@@ -1,30 +1,78 @@
 'use strict'
 
+
 class Calculator {
-  //write your code here
-  constructor () {
+  constructor(input){
+    this.input = input || 0
+    this.result = 0
+    this.pi = 3.14159265359
   }
-  add () {
+
+  add(operator){
+    this.process = this.input + operator
+    console.log(`${this.input} + ${operator} = ${this.finish()}`)
+    this.result = this.process
+    this.input = this.process
+    return this
   }
-  substract () {
+
+  substract(operator){
+    this.process = this.input - operator
+    console.log(`${this.input} - ${operator} = ${this.finish()}`)
+    this.result = this.process
+    this.input = this.process
+    return this
   }
-  multiply () {
+
+  multiply(operator){
+    this.process = this.input * operator
+    console.log(`${this.input} x ${operator} = ${this.finish()}`)
+    this.result = this.process
+    this.input = this.process
+    return this
   }
-  divide () {
+
+  divide(operator){
+    this.process = this.input / operator
+    console.log(`${this.input} / ${operator} = ${this.finish()}`)
+    this.result = this.process
+    this.input = this.process
+    return this
   }
-  square () {
+
+  square(operator){
+    this.process = Math.pow(this.input, operator)
+    console.log(`${this.input} ^ ${operator} = ${this.finish()}`)
+    this.result = this.process
+    this.input = this.process
+    return this
   }
-  squareRoot () {
+
+  root(operator){
+    if(!operator){
+      operator = 2
+    }
+    let root = String.fromCharCode(8730)
+    this.process = Math.pow(this.input, 1/operator)
+    console.log(`${operator} ${root} ${this.input} = ${this.finish()}`)
+    this.result = this.process
+    this.input = this.process
+    return this
+  }
+
+  finish(){
+    this.result = this.process
+    return this.result
   }
 }
 
-/** note : you can use several features from ecmascript, such as:
-* - Classes
-* - Default Parameters
-* - Destructured Assignment
-* - Template Literals
-* - Method Chaining
-*/
+var calculator = new Calculator(10)
+
+calculator.add(3).substract(2).multiply(2).square(2).root(2).add(calculator.pi)
+console.log(calculator);
+console.log(calculator.finish());
+
+
 
 module.exports = {
   Calculator
