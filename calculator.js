c62eeb18f891ab256b67cc3505707f15e38e8fec
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-12 10:48:38 +0700

    updated

--- calculator.js
+++ calculator.js
@@ -2,19 +2,90 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (number) {
+      this.result = number;
   }
-  add () {
+
+  add(num){
+      if(typeof num=='object'){
+          this.result += num.result;
+      }
+      else{
+          this.result+=num;
+      }
+      return this;
   }
-  substract () {
+
+  substract(num){
+      if(typeof num=='object'){
+          this.result -= num.result;
+      }
+      else{
+          this.result-=num;
+      }
+      return this;
   }
-  multiply () {
+
+  multiply(num){
+      if(typeof num=='object'){
+          this.result *= num.result;
+      }
+      else{
+          this.result*=num;
+      }
+      return this;
   }
-  divide () {
+
+  devide(num){
+      if(typeof num=='object'){
+          this.result /= num.result;
+      }
+      else{
+          this.result/=num;
+      }
+      return this;
   }
+
+  power(num){
+      if(typeof num=='object'){
+          this.result = Math.pow(num.result,num);;
+      }
+      else{
+          this.result = Math.pow(this.result,num);;
+      }
+      return this;
+  }
+
+  root(num){
+      if(typeof num=='object'){
+          this.result = Math.pow(num.result,1/num);;
+      }
+      else{
+          this.result = Math.pow(this.result,1/num);;
+      }
+      return this;
+  }
+
+  pi(){
+      return Math.PI;
+  }
+
   square () {
+      if(typeof num=='object'){
+          this.result = Math.pow(num.result,2);;
+      }
+      else{
+          this.result = Math.pow(this.result,2);;
+      }
   }
+
   squareRoot () {
+      if(typeof num=='object'){
+          this.result = Math.pow(num.result,0.5);;
+      }
+      else{
+          this.result = Math.pow(this.result,0.5);;
+      }
   }
 }
 
--- main.js
+++ main.js
@@ -2,4 +2,15 @@
 
 const {Calculator} = require("./calculator.js")
 
+
+
 // execute function on calculator.js in here
+let calculator = new Calculator(5);
+console.log(calculator.add(5).add(2).multiply(3).power(4).root(4).result);
+console.log("\nCalculate Circle: ");
+let radius = new Calculator(3);
+let luas = radius.multiply(radius).multiply(radius.pi()).result;
+console.log(luas);
+let tes = new Calculator(3);
+console.log("\nCalculate using own value: ");
+console.log(tes.add(5).add(tes).result);
