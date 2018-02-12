f131b0055b29fcefdf1a206183fe8ca5347d188d
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-12 11:09:02 +0700

    Kalkulator with method Chaining

--- calculator.js
+++ calculator.js
@@ -1,22 +1,72 @@
 'use strict'
 
-class Calculator {
-  //write your code here
-  constructor () {
+class Kalkulator{
+  
+  constructor(angkaAwal){
+   this.hasil = angkaAwal || 0; 
+   this.pi = 3.14;
   }
-  add () {
+  tambah(newNum){
+    if(newNum) {
+      this.hasil += newNum;  
+    } else {
+      this.hasil += this.hasil;  
+    }
+    return this;
   }
-  substract () {
+  kurang(newNum){
+    if(newNum) {
+      this.hasil -= newNum;  
+    } else {
+      this.hasil -= this.hasil;  
+    }
+    return this;
   }
-  multiply () {
+  kali(newNum){
+    if(newNum) {
+      this.hasil *= newNum;  
+    } else {
+      this.hasil *= this.hasil;  
+    }
+    return this;
   }
-  divide () {
+  bagi(newNum){
+    if(newNum) {
+      this.hasil /= newNum;  
+    } else {
+      this.hasil /= this.hasil;  
+    }
+    return this;
   }
-  square () {
+  pangkat(newNum){
+    var angkaKalian = this.hasil;
+    for(var i = 1; i < newNum; i++){
+      this.hasil = this.hasil * angkaKalian;  
+    }
+    return this;
   }
-  squareRoot () {
+  akarPangkat(newNum){
+    let angkaTerbesar = this.hasil;
+    for(var j = 0; j < angkaTerbesar; j++){
+      var angkaKalian = j;
+      var hasilPangkat = j;
+      for(var i = 1; i < newNum; i++){
+        hasilPangkat = hasilPangkat * angkaKalian;  
+      }
+      if(hasilPangkat === this.hasil){
+        this.hasil = j;  
+        break;
+      }
+    }
+    return this;
+  }
+  printHasil(){
+    console.log(`Hasil Perhitungan : ${this.hasil}`);
   }
 }
+
+
+  
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
@@ -27,5 +77,5 @@ class Calculator {
 */
 
 module.exports = {
-  Calculator
+  Kalkulator
 }
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict'
 
-const {Calculator} = require("./calculator.js")
+const {Kalkulator} = require("./calculator.js")
 
 // execute function on calculator.js in here
+let kalkulator = new Kalkulator();
+kalkulator.tambah(4).pangkat(3).akarPangkat(3).kali(2).kurang(1).bagi(2).printHasil();
