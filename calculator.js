5c4ccc885190b691dd4cc4c97eba7e13e74b1036
marcosumali <marco.sumali90@gmail.com>
2018-02-12 11:30:40 +0700

    add hasil

--- calculator.js
+++ calculator.js
@@ -2,21 +2,68 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num) {
+    this._phi = Math.PI;
+    this._num = num;
   }
-  add () {
+  add (num) {
+    this._numAdd = num;
+    if (this._numAdd != 0) {
+      this._num += this._numAdd;
+    }
+    return this;
   }
-  substract () {
+
+  substract (num) {
+    this._numSubst = num;
+    if (this._numSubst != 0) {
+      this._num -= this._numSubst;
+    }
+    return this;
   }
-  multiply () {
+
+  multiply (num) {
+    this._numMulti = num;
+    if (this._numMulti != 0) {
+      this._num *= this._numMulti;
+    }
+    return this;
   }
-  divide () {
+
+  divide (num) {
+    this._numDiv = num;
+    if (this._numDiv != 0) {
+      this._num /= this._numDiv;
+    }
+    return this;
   }
-  square () {
+
+  square (num) {
+    this._numSq = num;
+    if (this._numSq != 0) {
+      this._num = Math.pow(this._num, this._numSq);
+    }
+    return this;
   }
+
   squareRoot () {
+    if (this._num != 0) {
+      this._num = Math.sqrt(this._num);
+    }
+    return this;
   }
+
+  result () {
+    return `${this._num}`;
+  }
+
 }
+
+var result = new Calculator(2);
+
+console.log(result.add(2).substract(1).multiply(3).divide(3).square(2).squareRoot().result());
+console.log(result);
+
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
