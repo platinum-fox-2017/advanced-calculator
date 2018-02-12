54d23ee41370bacf39136356ee9a7b06b67607c6
adhiartaputra <adhiartaorabataputra@gmail.com>
2018-02-12 11:25:52 +0700

    Release 0

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ const {Calculator} = require("./calculator.js")
 // execute function on calculator.js in here
 let calc = new Calculator()
 
-console.log(calc.add(10).divide(2).square(2).multiply(4).substract(10).result());
+console.log(calc.add(10).divide(2).square(2).multiply(4).result());
